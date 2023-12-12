3a6afc8a9c6b47436610ba587ba601a6388b8280
nguyen thanh trung <ntttrung0701@gmail.com>
2023-12-13 00:14:36 +0700

    trung

---
+++ api/controllers/TourController.js
@@ -0,0 +1,106 @@
+const Tours = require("../models/Tours")
+const { JWT_PASS } = process.env
+const jwt = require('jsonwebtoken');
+const fs = require('fs')
+const path = require('path');
+const multer = require('multer');
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './publics/uploads/avatar');
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9) + path.extname(file.originalname);
+      cb(null, file.fieldname + '-' + uniqueSuffix);
+    }
+})
+
+exports.upload = multer({ storage: storage , 
+    fileFilter: function(req, file, cb){
+        if(!file.mimetype.includes('image')) return cb(new Error('I don\'t have a clue!'))
+
+        cb(null, true)
+}})
+exports.getAllTours = async (req, res) => {
+    try {
+        const tours = await Tours.find();
+        res.status(200).json({ status: 200, data: tours });
+    } catch (error) {
+        res.status(500).json({ message: 'Server error', error });
+    }
+}
+exports.CreateTour = async (req, res) => {
+    try {
+        const userId = req.user._id;
+        const user = await User.findById(userId);
+        if (!user || user.role !== 2) {
+            return res.status(403).json({ message: "Permission denied" });
+        }
+
+        const { name, thumbnail, originalPrice, discountPercentage, images, destination, active, schedule } = req.body;
+        const newTour = await Tours.create({
+            name,
+            thumbnail,
+            originalPrice,
+            discountPercentage,
+            images,
+            destination,
+            active,
+            schedule
+        });
+
+        res.status(200).json({ message: "Tour created successfully", tour: newTour });
+    } catch (error) {
+        res.status(500).json({ message: "Server error", error });
+    }
+}
+exports.updateTour = async (req, res) => {
+    try {
+        const userId = req.user._id;
+        const user = await User.findById(userId);
+        if (!user || user.role !== 2) {
+            return res.status(403).json({ message: "Permission denied" });
+        }
+
+        const { tourId, name, thumbnail, originalPrice, discountPercentage, images, destination, active, schedule } = req.body;
+        const existingTour = await Tours.findById(tourId);
+        if (!existingTour) {
+            return res.status(404).json({ message: "Tour not found" });
+        }
+        existingTour.name = name || existingTour.name;
+        existingTour.thumbnail = thumbnail || existingTour.thumbnail;
+        existingTour.originalPrice = originalPrice || existingTour.originalPrice;
+        existingTour.discountPercentage = discountPercentage || existingTour.discountPercentage;
+        existingTour.images = images || existingTour.images;
+        existingTour.destination = destination || existingTour.destination;
+        existingTour.active = active || existingTour.active;
+        existingTour.schedule = schedule || existingTour.schedule;
+
+        await existingTour.save();
+
+        res.status(200).json({ message: "Tour updated successfully", tour: existingTour });
+    } catch (error) {
+        res.status(500).json({ message: "Server error", error });
+    }
+}
+exports.deleteTour = async (req, res) => {
+    try {
+        const userId = req.user._id;
+        const user = await User.findById(userId);
+        if (!user || user.role !== 2) {
+            return res.status(403).json({ message: "Permission denied" });
+        }
+
+        const tourId = req.params.tourId;
+
+        const existingTour = await Tours.findById(tourId);
+        if (!existingTour) {
+            return res.status(404).json({ message: "Tour not found" });
+        }
+        await existingTour.remove();
+
+        res.status(200).json({ message: "Tour deleted successfully" });
+    } catch (error) {
+        res.status(500).json({ message: "Server error", error });
+    }
+}
+
--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -1,7 +1,8 @@
 const { signIn, signUp, getInforOneUser, getAllUserOrGetOne, changePassword, changeUserInfor, changeAvatar, upload } = require("../controllers/UserController");
+const { getBookings ,newBooking,ApproveBooking,cancelBooking } = require("../controllers/BookingController")
+const { getAllTours ,CreateTour, updateTour,deleteTour } = require("../controllers/TourController")
 const { checkLogin, CheckAdmin } = require("../middlewares/User");
 const router = require('express').Router();
-
 
 router.post('/api/sign-up', signUp);
 router.post('/api/sign-in', signIn);
