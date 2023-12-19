ac391728dbae488fc5dc431f0e119dc429cdf77b
nguyen thanh trung <ntttrung0701@gmail.com>
2023-12-19 22:14:16 +0700

    trung

--- api/controllers/ComController.js
+++ api/controllers/ComController.js
@@ -42,3 +42,21 @@ exports.postComment = async (req, res) => {
         res.status(500).json({ message: 'Server error', error });
     }
 }
+exports.deleteComment = async (req, res) => {
+    try {
+        const { commentId } = req.params;
+        const comment = await Comment.findById(commentId);
+
+        if (!comment) {
+            return res.status(404).json({ message: 'Comment not found' });
+        }
+        const userId = req.user._id;
+        if (req.user.role !== 2 && String(comment.userId) !== userId) {
+            return res.status(403).json({ message: 'You do not have permission to delete this comment' });
+        }
+        await Comment.findByIdAndDelete(commentId);
+        res.status(200).json({ message: 'Comment deleted successfully' });
+    } catch (error) {
+        res.status(500).json({ message: 'Server error', error });
+    }
+}
